72454679facd2d8682d9018db6258f703615ed17
pyler <pyler@azet.sk>
2015-09-27 19:26:06 +0200

    Support for 1034 - 1036

--- src/com/pyler/youtubebackgroundplayback/YouTubeBackgroundPlayback.java
+++ src/com/pyler/youtubebackgroundplayback/YouTubeBackgroundPlayback.java
@@ -11,20 +11,20 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class YouTubeBackgroundPlayback implements IXposedHookLoadPackage {
 	public static final String YOUTUBE_PACKAGE = "com.google.android.youtube";
 	public static final int[] YOUTUBE_VERSION = { 1002, 1003, 1004, 1005, 1006, 1008, 1009, 1010,
-		1011, 1012, 1013, 1014, 1015, 1016, 1018, 1019, 1020, 1021, 1024, 1025, 1028, 1029, 1031, 1032, 1033, 1037 };
+		1011, 1012, 1013, 1014, 1015, 1016, 1018, 1019, 1020, 1021, 1024, 1025, 1028, 1029, 1031, 1032, 1033, 1034, 1035, 1036, 1037 };
 	public static final String[] CLASS_BACKGROUND_PLAYER_SERVICE = { "com.google.android.apps.youtube.core.player.BackgroundPlayerService" , "com.google.android.libraries.youtube.player.decrepit.player.BackgroundPlayerService"};
 	public static final String[] CLASS_ENABLE_BACKGROUND_PLAYBACK = { "cti", "ctz", "cyj", "cyy", "cyk", "cyl", "cza", "cyj", "cym",
-		"cyc", "cyb", "cxa", "cxx", "cxw", "cxy", "dao", "dag", "dap", "dbq", "dcq", "dcg", "dbs", "ddh", "den", "dfb", "koj"};
+		"cyc", "cyb", "cxa", "cxx", "cxw", "cxy", "dao", "dag", "dap", "dbq", "dcq", "dcg", "dbs", "ddh", "den", "dfb", ,"kfj", "kgd", "knb" "koj"};
 	public static final String[] METHOD_ENABLE_BACKGROUND_PLAYBACK = { "u", "u", "u", "u", "u", "u", "u", "u", "v", "v", "x", "x",
-		"x", "x", "x", "y", "z", "A", "A", "A", "z", "z", "z", "C", "E", "B", "D"};
+		"x", "x", "x", "y", "z", "A", "A", "A", "z", "z", "z", "C", "E", "B", "B, "B", "B", "D"};
 	public static final String[] FIELD_PLAYBACK_CONTROL = { "i", "i", "i", "i", "i", "i", "i", "i", "i", "i", "i", "i",
-		"i", "i", "i", "i", "i", "i", "i", "i", "i", "f", "f", "f", "f", "f" };
+		"i", "i", "i", "i", "i", "i", "i", "i", "i", "f", "f", "f", "f", "f", "f", "f", "f" };
 	public static final String[] METHOD_RESTART_PLAYBACK = { "k", "k", "k", "k", "k", "j", "j", "j", "j", "j", "j", "j", "j", "j",
-		"j", "j", "j", "j", "j", "j", "j", "j", "i", "i", "i", "h" };
+		"j", "j", "j", "j", "j", "j", "j", "j", "i", "i", "i", "h", "h" , "h" , "h" };
 	public static final String[] FIELD_ENABLE_NOTIFICATION = { "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e",
-		"e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "d" };
+		"e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "d", "d", "d", "d"};
 	public static final String[] METHOD_NEXT_TRACK = { "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d",
-	"d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "e" };
+	"d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "e", "e", "e", "e" };
 	public static final String FIELD_TIME_MILLS = "a";
 	public static final String FIELD_LENGTH_MILLS = "b";
 	public int id;
