6f12d58517e24c99bfe849e52bdae5aaa3d23a96
pyler <pyler@azet.sk>
2015-09-27 19:11:06 +0200

    Added support for YT 10.37 (thanks iHelp101)

--- src/com/pyler/youtubebackgroundplayback/YouTubeBackgroundPlayback.java
+++ src/com/pyler/youtubebackgroundplayback/YouTubeBackgroundPlayback.java
@@ -11,18 +11,20 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class YouTubeBackgroundPlayback implements IXposedHookLoadPackage {
 	public static final String YOUTUBE_PACKAGE = "com.google.android.youtube";
 	public static final int[] YOUTUBE_VERSION = { 1002, 1003, 1004, 1005, 1006, 1008, 1009, 1010,
-		1011, 1012, 1013, 1014, 1015, 1016, 1018, 1019, 1020, 1021, 1024, 1025, 1028, 1029, 1031, 1032, 1033 };
-	public static final String BACKGROUND_PLAYER_SERVICE = "com.google.android.apps.youtube.core.player.BackgroundPlayerService";
+		1011, 1012, 1013, 1014, 1015, 1016, 1018, 1019, 1020, 1021, 1024, 1025, 1028, 1029, 1031, 1032, 1033, 1037 };
+	public static final String[] CLASS_BACKGROUND_PLAYER_SERVICE = { "com.google.android.apps.youtube.core.player.BackgroundPlayerService" , "com.google.android.libraries.youtube.player.decrepit.player.BackgroundPlayerService"};
 	public static final String[] CLASS_ENABLE_BACKGROUND_PLAYBACK = { "cti", "ctz", "cyj", "cyy", "cyk", "cyl", "cza", "cyj", "cym",
-		"cyc", "cyb", "cxa", "cxx", "cxw", "cxy", "dao", "dag", "dap", "dbq", "dcq", "dcg", "dbs", "ddh", "den", "dfb" };
+		"cyc", "cyb", "cxa", "cxx", "cxw", "cxy", "dao", "dag", "dap", "dbq", "dcq", "dcg", "dbs", "ddh", "den", "dfb", "koj"};
 	public static final String[] METHOD_ENABLE_BACKGROUND_PLAYBACK = { "u", "u", "u", "u", "u", "u", "u", "u", "v", "v", "x", "x",
-		"x", "x", "x", "y", "z", "A", "A", "A", "z", "z", "z", "C", "E", "B" };
+		"x", "x", "x", "y", "z", "A", "A", "A", "z", "z", "z", "C", "E", "B", "D"};
 	public static final String[] FIELD_PLAYBACK_CONTROL = { "i", "i", "i", "i", "i", "i", "i", "i", "i", "i", "i", "i",
-		"i", "i", "i", "i", "i", "i", "i", "i", "i", "f", "f", "f", "f" };
+		"i", "i", "i", "i", "i", "i", "i", "i", "i", "f", "f", "f", "f", "f" };
 	public static final String[] METHOD_RESTART_PLAYBACK = { "k", "k", "k", "k", "k", "j", "j", "j", "j", "j", "j", "j", "j", "j",
-		"j", "j", "j", "j", "j", "j", "j", "j", "i", "i", "i" };
-	public static final String FIELD_ENABLE_NOTIFICATION = "e";
-	public static final String METHOD_NEXT_TRACK = "d";
+		"j", "j", "j", "j", "j", "j", "j", "j", "i", "i", "i", "h" };
+	public static final String[] FIELD_ENABLE_NOTIFICATION = { "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e",
+		"e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "d" };
+	public static final String[] METHOD_NEXT_TRACK = { "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d",
+	"d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "e" };
 	public static final String FIELD_TIME_MILLS = "a";
 	public static final String FIELD_LENGTH_MILLS = "b";
 	public int id;
@@ -50,7 +52,7 @@ public class YouTubeBackgroundPlayback implements IXposedHookLoadPackage {
 			protected void afterHookedMethod(MethodHookParam param)
 					throws Throwable {
 				XposedHelpers.setBooleanField(param.thisObject,
-						FIELD_ENABLE_NOTIFICATION, true);
+						FIELD_ENABLE_NOTIFICATION[id], true);
 			}
 		};
 		XC_MethodHook advanceNextTrack = new XC_MethodHook() {
@@ -69,7 +71,7 @@ public class YouTubeBackgroundPlayback implements IXposedHookLoadPackage {
 								.getObjectField(param.thisObject,
 										FIELD_PLAYBACK_CONTROL[id]);
 						XposedHelpers.callMethod(playbackControl,
-								METHOD_NEXT_TRACK);
+								METHOD_NEXT_TRACK[id]);
 					}
 				} else {
 					prevTime = time;
@@ -91,7 +93,7 @@ public class YouTubeBackgroundPlayback implements IXposedHookLoadPackage {
 					METHOD_ENABLE_BACKGROUND_PLAYBACK[id],
 					XC_MethodReplacement.returnConstant(true));
 			XposedBridge.hookAllMethods(XposedHelpers.findClass(
-					BACKGROUND_PLAYER_SERVICE, lpparam.classLoader),
+					id < 1034 ? CLASS_BACKGROUND_PLAYER_SERVICE[0] : CLASS_BACKGROUND_PLAYER_SERVICE[1], lpparam.classLoader),
 					"handlePlaybackServiceException", restartPlayback);
 			XposedBridge.hookAllConstructors(XposedHelpers.findClass(
 					BACKGROUND_PLAYER_SERVICE, lpparam.classLoader),
