7dc06793170efafae7909a59391d86cc4c0ab50f
esgie <esgie84@gmail.com>
2015-11-02 11:02:40 +0100

    Support for yt 10.43.xx
    - support for yt 10.43.xx
    - additional settings hook

--- src/com/pyler/youtubebackgroundplayback/YouTubeBackgroundPlayback.java
+++ src/com/pyler/youtubebackgroundplayback/YouTubeBackgroundPlayback.java
@@ -11,14 +11,16 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class YouTubeBackgroundPlayback implements IXposedHookLoadPackage {
 
 	public static final String YOUTUBE_PACKAGE = "com.google.android.youtube";
-	public static final int[] YOUTUBE_VERSION = { 1076, 1077, 1078, 1079, 1080 };
-	public static final String[] CLASS_1 = { "knb", "koj", "kqp", "kul", "kys" };
-	public static final String[] METHOD_1_1 = { "J", "K", "O", "P", "P" };
-	public static final String[] FIELD_1_1 = { "f", "e", "e", "e", "e" };
-	public static final String[] FIELD_1_2 = { "d", "e", "e", "e", "e" };
-	public static final String[] CLASS_2 = { "iha", "ihy", "ijx", "ink", "iqq" };
-	public static final String[] METHOD_2_1 = { "a", "a", "a", "a", "a" };
-	public static final String[] FIELD_2_1 = { "c", "c", "c", "c", "c" };
+	public static final int[] YOUTUBE_VERSION = { 1080, 1083 };
+	public static final String[] CLASS_1 = { "kyr", "lco" };
+	public static final String[] METHOD_1_1 = { "P", "a" };
+	public static final String[] FIELD_1_1 = { "e", "d" };
+	public static final String[] FIELD_1_2 = { "e", "e" };
+	public static final String[] CLASS_2 = { "iqp", "iur" };
+	public static final String[] METHOD_2_1 = { "a", "a" };
+	public static final String[] FIELD_2_1 = { "c", "c" };
+	public static final String[] CLASS_3 = { "azq", "azl" };
+	public static final String[] METHOD_3_1 = { "c", "d" };
 	public int id;
 
 	@Override
@@ -55,6 +57,8 @@ public class YouTubeBackgroundPlayback implements IXposedHookLoadPackage {
 					}
 			});	
 			
+			XposedHelpers.findAndHookMethod(CLASS_3[id],lpparam.classLoader,METHOD_3_1[id],XC_MethodReplacement.returnConstant("on"));
+			
 		} else {
 			XposedBridge.log("This YouTube version is not supported yet.");
 		}
@@ -67,3 +71,4 @@ public class YouTubeBackgroundPlayback implements IXposedHookLoadPackage {
 		return -1;
 	}
 }
+
