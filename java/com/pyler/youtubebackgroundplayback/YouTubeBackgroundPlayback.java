a6b2dea06a08fc007dc5d0d1ed0214b153dc709b
Valters Jansons <valter.jansons@gmail.com>
2016-04-23 15:38:31 +0300

    Rework the codebase
    The code has been refactored, moved around and messed with to make
    the whole flow more readable, cleaner and lovelier in general.

--- java/com/pyler/youtubebackgroundplayback/YouTubeBackgroundPlayback.java
+++ java/com/pyler/youtubebackgroundplayback/YouTubeBackgroundPlayback.java
@@ -1,6 +1,7 @@
 package com.pyler.youtubebackgroundplayback;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -90,69 +91,61 @@ public class YouTubeBackgroundPlayback implements IXposedHookLoadPackage {
 	public static final String[] METHOD_5 = { "isDogfoodOrDevBuild" };
 
 	@Override
-	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+	public void handleLoadPackage(final LoadPackageParam lpparam) throws PackageManager.NameNotFoundException {
 		if (!lpparam.packageName.equals(APP_PACKAGE)) return;
 
 		final ClassLoader loader = lpparam.classLoader;
 
-		// check if deobfuscated class name is present
-		boolean isDeobfuscatedCode = true;
-		try {
-			loader.loadClass(CLASS_1[0]);
-		} catch (Exception e) {
-			isDeobfuscatedCode = false;
-		}
-
-		final Object activityThread = callStaticMethod(
-			findClass("android.app.ActivityThread", null), "currentActivityThread");
-		final Context context = (Context) callMethod(activityThread, "getSystemContext");
-
 		// get classes/methods/fields names index
-		final int i = getVersionIndex(context.getPackageManager()
-			.getPackageInfo(APP_PACKAGE, 0).versionCode / 1000, isDeobfuscatedCode);
+		final int i = getVersionIndex(loader);
 		if (i == -1) {
-			log("Your version of the YouTube app is not yet supported.");
+			log("Could not enable background playback for the YouTube app. Your installed version of it is not supported.");
 			return;
 		}
 
 		// hooks
 		findAndHookMethod(CLASS_1[i], loader, METHOD_1[i], new XC_MethodHook() {
 			@Override
-			protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
+			protected void beforeHookedMethod(final MethodHookParam param) {
 				setBooleanField(getObjectField(param.thisObject, FIELD_1[i]), SUBFIELD_1[i], true);
 			}
 		});
 
 		findAndHookMethod(CLASS_2[i], loader, METHOD_2[i], new XC_MethodHook() {
 			@Override
-			protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
+			protected void beforeHookedMethod(final MethodHookParam param) {
 				setBooleanField(param.thisObject, FIELD_2[i], true);
 			}
 		});
 
 		findAndHookMethod(CLASS_3[i], loader, METHOD_3[i], returnConstant("on"));
 
-		if (isDeobfuscatedCode) {
-			// hooks for methods only for deobfuscated releases
+		if (i == 0) {
+			// hook specific methods for unobfuscated releases
 			findAndHookMethod(CLASS_4[0], loader, METHOD_4[0], returnConstant(true));
+
 			findAndHookMethod(CLASS_5[0], loader, METHOD_5[0], returnConstant(true));
 		}
-
 	}
 
-	// returns 0 for deobfuscated code and positive integer for obfuscated
-	private int getVersionIndex(final int versionCode, final boolean isDeobfuscatedCode) {
-		if (isDeobfuscatedCode) {
+	// returns 0 for unobfuscated code and positive integer for obfuscated
+	private int getVersionIndex(final ClassLoader loader) throws PackageManager.NameNotFoundException {
+		try {
+			// check if the app is unobfuscated
+			loader.loadClass(CLASS_1[0]);
 			return 0;
-		}
-
-		for (int i = 1; i < APP_VERSIONS.length; i++) {
-			if (APP_VERSIONS[i] == versionCode) {
-				return i;
+		} catch (Exception e) {
+			// look through all the known app versions
+			final Object activityThread = callStaticMethod(findClass("android.app.ActivityThread", null), "currentActivityThread");
+			final Context context = (Context) callMethod(activityThread, "getSystemContext");
+			final int versionCode = context.getPackageManager().getPackageInfo(APP_PACKAGE, 0).versionCode / 1000;
+			for (int i = 1; i < APP_VERSIONS.length; i++) {
+				if (APP_VERSIONS[i] == versionCode) {
+					return i;
+				}
 			}
+			return -1;
 		}
-
-		return -1;
 	}
 
 }
